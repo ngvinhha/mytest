278e48495bb6509ed340e1fb8a873bfd7e68465f
ngvinhha <ngvinhha80@gmail.com>
2021-01-24 19:23:23 +0700

    mern youtube

--- src/components/create-exercise.js
+++ src/components/create-exercise.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
-import DatePicker from "react-datepicker"
-import "react-datepicker/dist/react-datepicker.css"
+import React, { Component } from 'react';
+import axios from 'axios'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export class CreateExercise extends Component {
     constructor(props){
@@ -12,6 +13,8 @@ export class CreateExercise extends Component {
         this.onChangeDate=this.onChangeDate.bind(this)
         this.onSubmit=this.onSubmit.bind(this)
 
+        this.inputRef=React.createRef()
+
         this.state={
             username:'',
             description:'',
@@ -21,11 +24,20 @@ export class CreateExercise extends Component {
         }
     }
 
+    focusonInputField=()=>{
+        this.inputRef.current.focus()
+    }
+
     componentDidMount(){
-        this.setState({
-            users:['test user'],
-            username: 'test user'
-        })
+        axios.get('http://localhost:5000/users/')
+            .then(response=>{
+                if(response.data.length>0){
+                    this.setState({
+                        users: response.data.map(user=>user.username),
+                        username:response.data[0].username
+                    })
+                }
+            })
     }
 
     onChangeUsername(e){
@@ -64,6 +76,10 @@ export class CreateExercise extends Component {
 
         console.log(exercise)
 
+        axios.post('http://localhost:5000/exercises/add',exercise)
+            .then(res=>console.log(res.data));
+
+
         window.location='/'
     }
 
@@ -74,7 +90,7 @@ export class CreateExercise extends Component {
                 <form onSubmit={this.onSubmit}>
                     <div className="form-group">
                         <label>Username:</label>
-                        <select ref="userInput"
+                        <select ref={this.inputRef}
                             required
                             className="form-control"
                             value={this.state.username}
